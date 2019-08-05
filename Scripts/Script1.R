97d029de8cc35a5adad8461e98b26c10c021a041
alfonso <alfons.noguer@gmail.com>
2019-08-05 17:29:29 +0200

    more work on the floor prediction

--- Scripts/Script1.R
+++ Scripts/Script1.R
@@ -1,5 +1,5 @@
 pacman::p_load(readr,caret,tidyverse,ggplot2,plotly, lubridate, ranger,
-               DataExplorer)
+               DataExplorer,ggbiplot)
 
 
 # Coments -----------------------------------------------------------------
@@ -14,9 +14,8 @@ pacman::p_load(readr,caret,tidyverse,ggplot2,plotly, lubridate, ranger,
 # Get and preporcess the data ------------------------------------------------
 
 train <- read_csv("Data/trainingData.csv")
-train$is.train <- 1
 
-create_report(train %>% select(-contains("WAP")))
+# create_report(train %>% select(-contains("WAP")))
 
 model_building <- train %>% select(contains("WAP"),BUILDINGID,FLOOR,LONGITUDE,
                                    LATITUDE)
@@ -28,7 +27,7 @@ test <- read_csv("Data/validationData.csv")
 unique(test$PHONEID) %in% unique(test$PHONEID)
 
 summary(test%>% select(-contains('WAP')))
-test$is.train <- 0
+# test$is.train <- 0
 fulldata <- rbind(train,test)
 glimpse(fulldata)
 
@@ -43,24 +42,26 @@ fulldata[,tail(names(fulldata),n = 12)]
 fulldata$TIMESTAMP <- as_datetime(fulldata$TIMESTAMP)
 trainWAP <- model_building %>% select(contains("WAP"))
 
-tc <- trainControl(method = "CV",number = 10)
-
-model_building_01 <- train(x = model_building %>% select(contains("WAP")),
-                           y = as.factor(model_building$BUILDINGID),
-                           method = "rf",
-                           trControl = tc,
-                           tuneLength = 4
-                           )
-
+# tc <- trainControl(method = "CV",number = 10)
+# 
+# model_building_01 <- train(x = model_building %>% select(contains("WAP")),
+#                            y = as.factor(model_building$BUILDINGID),
+#                            method = "rf",
+#                            trControl = tc,
+#                            tuneLength = 4
+#                            )
 
 # Ranger for MVP ----------------------------------------------------------
-# building
+#predicting Building
+{# building
 data_building <- model_building %>% select(contains("WAP"),BUILDINGID) 
 data_building$BUILDINGID <- as.factor(data_building$BUILDINGID)
 ranger_model_building <- ranger(BUILDINGID ~ ., data = data_building)
 pred.ranger.building <- predict(ranger_model_building, data = test) 
 
-table(test$BUILDINGID, pred.ranger.building$predictions)
+table(test$BUILDINGID, pred.ranger.building$predictions)}
+# Predicting FLOOR
+{
 # floor building 0
 data_building_0_floor <- model_building %>% filter(BUILDINGID == 0) %>%
   select(contains("WAP"),FLOOR)
@@ -109,50 +110,182 @@ confusionMatrix(as.factor(test_building0floor$FLOOR),
 }
 
 # floor building 1
+test_building1WAP<- test %>% filter(BUILDINGID == 1) %>%
+                       select(starts_with("WAP"))
+test_building1floor <- (test%>% filter(BUILDINGID == 1) %>%
+                          select(FLOOR))
 data_building_1_floor <- model_building %>% filter(BUILDINGID == 1) %>%
   select(contains("WAP"),FLOOR)
+
+# WE BIN THE INTESITY SIGNALS
+intesidad <- function(x){
+  return(findInterval(x,vec = c(-90,-80,-70,-60,-30,0)))
+}
+data_building_1_floor_binned <- as_tibble(apply(data_building_1_floor %>%
+                                  select(contains("WAP")),
+                                2,
+                                intesidad))
+# las columnes con 100 y menos de -90 son iguales ya que no conectan a internet
+data_building_1_floor_binned[data_building_1_floor_binned==6]<-0
+data_building_1_floor_binned$FLOOR <- data_building_1_floor$FLOOR
+# we remove the 0 variance columns
+ZerobarB1 <- nearZeroVar(data_building_1_floor,saveMetrics = TRUE)
+data_building_1_floor <- data_building_1_floor[,!ZerobarB1$zeroVar]
+
+ZerobarB1_BINNED <- nearZeroVar(data_building_1_floor_binned,
+                                saveMetrics = TRUE)
+data_building_1_floor_binned <- data_building_1_floor_binned[
+  ,!ZerobarB1_BINNED$zeroVar]
+
+# modelos sin PCA
 ranger_model_building_1_floor <- ranger(FLOOR ~ .,
                                         data = data_building_1_floor)
 pred.ranger.building_1_floor <- predict(ranger_model_building_1_floor,
                                         data = test %>% 
                                           filter(BUILDINGID == 1))
-test_building1floor <- (test%>% filter(BUILDINGID == 1) %>%
-  select(FLOOR))
 confusionMatrix(as.factor(test_building1floor$FLOOR),
                 factor(pred.ranger.building_1_floor$predictions))
-# Confusion Matrix and Statistics
+# Confusion Matrix and Statistics 
 {
 #   Confusion Matrix and Statistics
 #   
 #   Reference
 #   Prediction  0  1  2  3
-        #   0  22  2  5  1
-        #   1   4 91 45  3
-        #   2   0  1 77  9
-        #   3   0  0  4 43
+#   0 24  1  5  0
+#   1  1 93 46  3
+#   2  0  1 78  8
+#   3  0  0  4 43
 #   
 #   Overall Statistics
 #   
-#   Accuracy : 0.759           
-#   95% CI : (0.7071, 0.8057)
-#   No Information Rate : 0.4267          
+#   Accuracy : 0.7752          
+#   95% CI : (0.7244, 0.8207)
+#   No Information Rate : 0.4332          
 #   P-Value [Acc > NIR] : < 2.2e-16       
 #   
-#   Kappa : 0.6558          
+#   Kappa : 0.6781          
 #   
-#   Mcnemar's Test P-Value : 8.572e-10       
+#   Mcnemar's Test P-Value : NA              
 # 
 # Statistics by Class:
 # 
 #                      Class: 0 Class: 1 Class: 2 Class: 3
-# Sensitivity           0.84615   0.9681   0.5878   0.7679
-# Specificity           0.97153   0.7559   0.9432   0.9841
-# Pos Pred Value        0.73333   0.6364   0.8851   0.9149
-# Neg Pred Value        0.98556   0.9817   0.7545   0.9500
-# Prevalence            0.08469   0.3062   0.4267   0.1824
-# Detection Rate        0.07166   0.2964   0.2508   0.1401
+# Sensitivity           0.96000   0.9789   0.5865   0.7963
+# Specificity           0.97872   0.7642   0.9483   0.9842
+# Pos Pred Value        0.80000   0.6503   0.8966   0.9149
+# Neg Pred Value        0.99639   0.9878   0.7500   0.9577
+# Prevalence            0.08143   0.3094   0.4332   0.1759
+# Detection Rate        0.07818   0.3029   0.2541   0.1401
 # Detection Prevalence  0.09772   0.4658   0.2834   0.1531
-# Balanced Accuracy     0.90884   0.8620   0.7655   0.8760
+# Balanced Accuracy     0.96936   0.8715   0.7674   0.8902
+}
+# modelo sin PCA con binning
+
+ranger_model_building_1_floor_bin <- ranger(FLOOR ~ .,
+                                        data = data_building_1_floor_binned)
+pred.ranger.building_1_floor_bin <- predict(ranger_model_building_1_floor_bin,
+                                        data = test %>% 
+                                          filter(BUILDINGID == 1))
+confusionMatrix(as.factor(test_building1floor$FLOOR),
+                factor(pred.ranger.building_1_floor_bin$predictions))
+#   Confusion Matrix and Statistics
+{
+#   
+#   Reference
+#   Prediction  0  1  2  3
+#   0 15  9  6  0
+#   1  4 87 50  2
+#   2  0  2 71 14
+#   3  0  1 19 27
+#   
+#   Overall Statistics
+#   
+#   Accuracy : 0.6515          
+#   95% CI : (0.5953, 0.7047)
+#   No Information Rate : 0.4756          
+#   P-Value [Acc > NIR] : 4.083e-10       
+#   
+#   Kappa : 0.4931          
+#   
+#   Mcnemar's Test P-Value : NA              
+# 
+# Statistics by Class:
+# 
+#                      Class: 0 Class: 1 Class: 2 Class: 3
+# Sensitivity           0.78947   0.8788   0.4863  0.62791
+# Specificity           0.94792   0.7308   0.9006  0.92424
+# Pos Pred Value        0.50000   0.6084   0.8161  0.57447
+# Neg Pred Value        0.98556   0.9268   0.6591  0.93846
+# Prevalence            0.06189   0.3225   0.4756  0.14007
+# Detection Rate        0.04886   0.2834   0.2313  0.08795
+# Detection Prevalence  0.09772   0.4658   0.2834  0.15309
+# Balanced Accuracy     0.86870   0.8048   0.6935  0.77607
+}
+
+# Modelo PCA
+
+# we run a PCA
+prin_comp <- prcomp(data_building_1_floor %>% select(-FLOOR))
+# biplot(prin_comp, scale = 0)
+#compute standard deviation of each principal component
+std_dev <- prin_comp$sdev
+#compute variance
+pr_var <- std_dev^2
+#proportion of variance explained
+prop_varex <- pr_var/sum(pr_var)
+#scree plot
+plot(prop_varex, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     type = "b")
+#cumulative scree plot
+plot(cumsum(prop_varex), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
+# 91 componentes explican aproximadamente el 95% de la variancia
+B1_floor_PCA <- data.frame(data_building_1_floor %>% select(FLOOR),
+                           prin_comp$x)
+# WE MODEL
+ranger_model_building_1_floor_PCA <- ranger(FLOOR ~ .,
+                                        data = B1_floor_PCA[,1:91])
+B1_PCA_TEST <-  as.data.frame( predict(prin_comp, newdata = test_building1WAP))
+
+pred.ranger.building_1_floor_PCA <- predict(ranger_model_building_1_floor_PCA,
+                                        data = B1_PCA_TEST)
+confusionMatrix(as.factor(test_building1floor$FLOOR),
+                factor(pred.ranger.building_1_floor_PCA$predictions))
+# Confusion Matrix and Statistics 
+{
+#   Confusion Matrix and Statistics
+#   
+#   Reference
+#   Prediction  0  1  2  3
+#   0 23  2  4  1
+#   1  9 81 52  1
+#   2  0  1 74 12
+#   3  0  0  9 38
+#   
+#   Overall Statistics
+#   
+#   Accuracy : 0.7036          
+#   95% CI : (0.6491, 0.7541)
+#   No Information Rate : 0.4528          
+#   P-Value [Acc > NIR] : < 2.2e-16       
+#   
+#   Kappa : 0.5814          
+#   
+#   Mcnemar's Test P-Value : 4.589e-11       
+# 
+# Statistics by Class:
+# 
+#                      Class: 0 Class: 1 Class: 2 Class: 3
+# Sensitivity           0.71875   0.9643   0.5324   0.7308
+# Specificity           0.97455   0.7220   0.9226   0.9647
+# Pos Pred Value        0.76667   0.5664   0.8506   0.8085
+# Neg Pred Value        0.96751   0.9817   0.7045   0.9462
+# Prevalence            0.10423   0.2736   0.4528   0.1694
+# Detection Rate        0.07492   0.2638   0.2410   0.1238
+# Detection Prevalence  0.09772   0.4658   0.2834   0.1531
+# Balanced Accuracy     0.84665   0.8431   0.7275   0.8477
 }
 
 
@@ -204,7 +337,7 @@ confusionMatrix(as.factor(test_building2floor$FLOOR),
 # Balanced Accuracy     0.90326   0.9506   0.9067   0.7721  0.95184
 }
 
-
+}
 
 # floor building 1 error analysis -----------------------------------------
 
